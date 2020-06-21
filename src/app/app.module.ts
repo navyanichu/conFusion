ba46e4e09ecd10b63ec2fbbf06caa8bf6e888ae7
Navya Divakar <navyasreyas22@gmail.com>
2020-06-21 21:46:15 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {MatCardModule} from '@angular/material/card'
 import {MatButtonModule} from '@angular/material/button'
 
 import { DishdetailComponent } from './dishdetail/dishdetail.component'
-
+import { DishService } from './services/dish.service'
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +32,10 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component'
     MatButtonModule,
     MatCardModule,
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
